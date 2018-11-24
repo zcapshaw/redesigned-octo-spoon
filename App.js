2bbdc8c37b6c62838605a2a5cb235b9db534ddf8
Zach Capshaw <zcapshaw@gmail.com>
2018-11-24 12:27:12 -0700

    Allow sign out

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './src/components/common';
+import { Header, Button, Spinner } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 export default class App extends React.Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -26,11 +26,34 @@ export default class App extends React.Component {
     });
   }
 
+  //if user is loggedin, show the logout page
+  //if we don't know user state, show spinner
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ height: 45 }}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={{ marginTop: 100 }}>
+            <Spinner size='large' />
+          </View>
+        );
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText={'Authentication'} />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -30,7 +30,7 @@ const styles = {
     borderWidth: 1,
     borderColor: '#007aff',
     marginLeft: 5,
-    marginRight: 5
+    marginRight: 5,
   }
 };
 
